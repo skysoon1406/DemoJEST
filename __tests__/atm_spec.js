6f3da7e6a9974de8f0bc7f5e6845183fba76d148
Soon <skysoon1406@gmail.com>
2025-05-05 12:33:53 +0800

    feat: add test : negative deposit rejected

--- __tests__/atm_spec.js
+++ __tests__/atm_spec.js
@@ -5,7 +5,9 @@ class ATM {
     }
 
     deposit(amount){
-        this.currentBalance = this.currentBalance + amount
+        if (amount > 0 ){
+            this.currentBalance = this.currentBalance + amount
+        }
     }
 
     balance() {
@@ -28,3 +30,16 @@ test("可以存錢2",() => {
     expect(atm.balance()).toBe(5);
 })
 // 測試如果沒有帶餘額 ，測試預設值是否生效
+
+test("不可存0 或小於0 的金額",() => {
+    // 3A 原則： Arrange安排  Act行動  Assert評估（預期的結果）
+
+    // Arrange安排 
+    const atm = new ATM(10);
+    
+    // Act行動  = 假設存了 負數金額
+    atm.deposit(-5);
+
+    // Assert評估 =  存款金額不變
+    expect(atm.balance()).toBe(10);
+})
--- __tests__/math_spec.js
+++ __tests__/math_spec.js
@@ -6,7 +6,7 @@ function sum(a,b)  {
 test("1+2 = 3",() => {
     expect(sum(1,2)).toBe(3);
 });  
-// 先寫測試 （規格） ： 預期 sum 這個function 帶參數 1,2 ， toBe(3)
+
 
 test("2+2 = 4",() => {
     expect(sum(2,2)).toBe(4);
