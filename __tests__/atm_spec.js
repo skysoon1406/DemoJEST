a0f728b5a73ff6033bc0c8db4cd1019b936cd7fc
Soon <skysoon1406@gmail.com>
2025-05-05 12:22:27 +0800

    feat: add test : default balance

--- __tests__/atm_spec.js
+++ __tests__/atm_spec.js
@@ -1,6 +1,6 @@
 
 class ATM {
-    constructor(currentBalance) {
+    constructor(currentBalance = 0) {
         this.currentBalance = currentBalance
     }
 
@@ -20,3 +20,11 @@ test("可以存錢",() => {
     expect(atm.balance()).toBe(15);
 })
 // 先寫測試 （規格） ： 宣告 atm = ATM(原來餘額=5) ， atm.deposit(5)以後，預期 atm.balance 是15
+
+
+test("可以存錢2",() => {
+    const atm = new ATM();
+    atm.deposit(5);
+    expect(atm.balance()).toBe(5);
+})
+// 測試如果沒有帶餘額 ，測試預設值是否生效
