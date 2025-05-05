8ab225ab7b07162037d382f42db43aed1c1347dc
Soon <skysoon1406@gmail.com>
2025-05-05 12:42:26 +0800

    feat: deposit test keep into describe

--- __tests__/atm_spec.js
+++ __tests__/atm_spec.js
@@ -16,31 +16,35 @@ class ATM {
     }
 }
 
-
-test("可以存錢",() => {
-    const atm = new ATM(10);
-    atm.deposit(5);
-    expect(atm.balance()).toBe(15);
-})
-// 先寫測試 （規格） ： 宣告 atm = ATM(原來餘額=5) ， atm.deposit(5)以後，預期 atm.balance 是15
-
-
-test("可以存錢2",() => {
-    const atm = new ATM();
-    atm.deposit(5);
-    expect(atm.balance()).toBe(5);
-})
-// 測試如果沒有帶餘額 ，測試預設值是否生效
-
-test("不可存0 或小於0 的金額",() => {
-    // 3A 原則： Arrange安排  Act行動  Assert評估（預期的結果）
-
-    // Arrange安排 
-    const atm = new ATM(10);
+// 可以用 describe 的把下面的Test全部包起來：
+describe("存錢相關功能",() => {
+    test("可以存錢",() => {
+        const atm = new ATM(10);
+        atm.deposit(5);
+        expect(atm.balance()).toBe(15);
+    })
+    // 先寫測試 （規格） ： 宣告 atm = ATM(原來餘額=5) ， atm.deposit(5)以後，預期 atm.balance 是15
     
-    // Act行動  = 假設存了 負數金額
-    atm.deposit(-5);
+    
+    test("可以存錢2",() => {
+        const atm = new ATM();
+        atm.deposit(5);
+        expect(atm.balance()).toBe(5);
+    })
+    // 測試如果沒有帶餘額 ，測試預設值是否生效
+    
+    
+    test("不可存0 或小於0 的金額",() => {
+        // 3A 原則： Arrange安排  Act行動  Assert評估（預期的結果）
+    
+        // Arrange安排 
+        const atm = new ATM(10);
+        
+        // Act行動  = 假設存了 負數金額
+        atm.deposit(-5);
+    
+        // Assert評估 =  存款金額不變
+        expect(atm.balance()).toBe(10);
+    })
 
-    // Assert評估 =  存款金額不變
-    expect(atm.balance()).toBe(10);
 })
